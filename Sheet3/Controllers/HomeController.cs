aa563a3b322abe7c8f994a713bf0d3f5b858c1d8
Rémi Jonathan Choquette <contact@remijonathan.com>
2019-09-17 23:46:53 -0400

    Created Index View and Model

---
+++ Sheet3/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ContactBLL;
+using Entities;
+using Sheet3.Models;
+
+namespace Sheet3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public Model myModel = new Model();
+        // GET: Home
+        public ActionResult Index()
+        {
+            var records = myModel.GetList();
+            return View(records);
+        }
+    }
+}
---
+++ Sheet3/Models/Model.cs
@@ -0,0 +1,18 @@
+﻿using ContactBLL;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sheet3.Models
+{
+    public class Model
+    {
+        private Business bll = new Business();
+        public List<Contact> GetList()
+        {
+            return bll.GetContacts();
+        }
+    }
+}
