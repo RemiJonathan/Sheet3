bdf7fd8ae2f39cb00c38b0b0c5754471495e8328
Rémi Jonathan Choquette <contact@remijonathan.com>
2019-09-17 21:54:44 -0400

    Created Contact Object

--- Entities/Contact.cs
+++ Entities/Contact.cs
@@ -8,5 +8,14 @@ namespace Entities
 {
     public class Contact
     {
+        public int Id { get; set; }
+        public string FName { get; set; }
+        public string LName { get; set; }
+        public string CellNum { get; set; }
+        public string HomeNum { get; set; }
+        public string WorkNum { get; set; }
+        public string Address { get; set; }
+        public string Notes { get; set; }
+        public bool isActive { get; set; }
     }
 }
